81d8b9c9331f3f4c29758c1a8b7c31c9f53da874
david wasem <davidwasem8@gmail.com>
2025-09-17 23:10:00 +0200

    adding home page

---
+++ lib/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page',
+         style: TextStyle(color: Colors.white,
+         fontSize: 30.0)),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: const Center(
+        child: Text('Welcome to the Home Page!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.favorite, color: Colors.white),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
