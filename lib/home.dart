76daad7f78695d1bfdbe0e9e338e90efbd5c89c1
david wasem <davidwasem8@gmail.com>
2025-09-18 13:04:33 +0200

    adding assets

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class home extends StatelessWidget {
   const home({super.key});
 
@@ -12,14 +13,27 @@ class home extends StatelessWidget {
          fontSize: 30.0)),
         backgroundColor: Colors.deepPurple,
       ),
-      drawer: Drawer(),
+      drawer: Drawer(width: 250,
+      backgroundColor: Color.fromARGB(255, 181, 138, 255),),
       body: const Center(
-        child: Text('Welcome to the Home Page!'),
+        child: Column(children: [
+          Image(image:  NetworkImage("https://img.freepik.com/free-vector/mobile-browsers-concept-illustration_114360-1267.jpg"),
+          height: 300,
+          width: 400,),
+          Text('Welcome to the Home Page!'),
+        ],),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Colors.deepPurple,
         child: const Icon(Icons.favorite, color: Colors.white),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.deepPurple,
+        child: IconButton(
+          icon: const Icon(Icons.add, color: Colors.white),
+          onPressed: () {},
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
